18b3217e3b2378c3d62e1a68b4723f34b452ddd0
matheus <matheus@tradenamao.com.br>
2021-01-15 17:02:45 -0300

    WIP: testing how to formate dates

--- app/controllers/per_period_controller.rb
+++ app/controllers/per_period_controller.rb
@@ -1,4 +1,15 @@
 class PerPeriodController < ApplicationController
   def index
+    @questions = Question.select(Arel.star).joins(
+      Question.arel_table.join(QuestionsAccess.arel_table).on(
+        QuestionsAccess.arel_table[:question_id].eq(Question.arel_table[:id])
+      ).join_sources
+    ).limit(10)
+
+    # puts(QuestionsAccess.group("month(created_at)").count)
+    # puts(@questions.group("month(created_at)").count)
+    for question in @questions do
+      puts(question['created_at'].to_s)
+    end
   end
 end
--- app/controllers/trending_controller.rb
+++ app/controllers/trending_controller.rb
@@ -6,10 +6,7 @@ class TrendingController < ApplicationController
     if params['all'] == "true" then
       @questions = Question.order(:discipline).all
     else
-      params = {}
       @questions = Question.select(:discipline, :daily_access).distinct.order(:discipline).first(10)
-    end
-    for q in @questions do
     end
   end
 end
